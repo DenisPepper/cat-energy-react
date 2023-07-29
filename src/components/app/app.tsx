26f1b7c885b0e60fb54846f6ef7bcf4d34690bee
DenisPepper <thech@yandex.ru>
2023-07-29 22:57:52 +0300

    Добавит черновики страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,21 +1,17 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {HomePage} from "../../pages/home-page/home-page";
 import {CatalogPage} from "../../pages/catalog-page/catalog-page";
-import {Footer} from "../footer/footer";
-import {Header} from "../header/header";
 import {DevPage} from "../../pages/dev-page/dev-page";
 
 export const App = () => {
     return (
         <div className='page-wrapper'>
             <BrowserRouter>
-                <Header/>
                 <Routes>
                     <Route path='/' element={<HomePage/>}/>
                     <Route path='/catalog' element={<CatalogPage/>}/>
                     <Route path='/dev' element={<DevPage/>}/>
                 </Routes>
-                <Footer/>
             </BrowserRouter>
         </div>
     );
